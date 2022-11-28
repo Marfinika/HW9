a7603754fce47e76a520f7e25cd849140a37becc
Ivan Zuev <ivan.zuev96@yandex.ru>
2022-11-28 18:47:16 +0300

    first commit

---
+++ src/main/java/org/netology/FormDate.java
@@ -0,0 +1,7 @@
+package org.netology;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/org/netology/Main.java
@@ -0,0 +1,18 @@
+package org.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "1234 № 98765432";
+        post.patronymic = "Федорович";
+        post.phone = "+7 (905)-999-01-23";
+        post.surname = "Крузенштерн";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
+}
